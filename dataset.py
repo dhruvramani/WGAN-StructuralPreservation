c30683870f6a96f0f340112e87cd989310a19458
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-16 20:16:10 +0530

    checking

--- dataset.py
+++ dataset.py
@@ -65,6 +65,8 @@ class AugumentedDataset(Dataset):
 
     def __getitem__(self, idx):
         real_img = self.real_data.__getitem__(idx)
+        print(real_img)
+        _ = input()
         fake_img = torch.Tensor(self.earser(real_img.numpy()))
 
         return torch.stack((real_img, fake_img)), torch.Tensor([1, 0])
