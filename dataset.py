0760ebfd8b7085ae2faa7b0f21bcdc559b2ea1cd
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 14:12:35 +0530

    debug

--- dataset.py
+++ dataset.py
@@ -68,7 +68,10 @@ class AugumentedDataset(Dataset):
         return int(self.real_data.__len__())
 
     def __getitem__(self, idx):
-        real_img = self.real_data.__getitem__(idx)[0]
+        real_img = self.real_data.__getitem__(idx)
+        print(real_img.shape)
+        _ = input()
+        real_img = real_img[0]
         real_np = np.array(real_img)
         fake_img = torch.Tensor(self.earser(real_np))
         real_img = torch.Tensor(real_np)
