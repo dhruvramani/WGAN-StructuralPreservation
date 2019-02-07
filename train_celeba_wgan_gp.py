62f39f51662eb1d9f9c7dab45530dd6ca8682ad7
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 14:30:13 +0530

    debug

--- train_celeba_wgan_gp.py
+++ train_celeba_wgan_gp.py
@@ -79,6 +79,7 @@ def train_aesthecity():
         train_loss, accu1 = 0.0, 0.0
         for _ in range(step, le):
             real_img, label1, fake_img, label0 = next(dataloader)
+            print(real_img.shape, fake_img.shape)
             imgs, labels = torch.stack((real_img, fake_img)), torch.stack((label1, label0))
             print(imgs.shape)
             _ = input()
