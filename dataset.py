280c4ddfef0b799b50ed98f2898456cabe2518f2
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 15:38:04 +0530

    foo

--- dataset.py
+++ dataset.py
@@ -15,7 +15,7 @@ from torch.utils.data import Dataset, DataLoader
 import utils
 
 _DATASET_PATH = '/home/nevronas/dataset/img_align_celeba'
-
+'''
 def get_random_eraser(p=1, area_ratio_range=[0.02, 0.4], min_aspect_ratio=0.3, max_attempt=20):
     sl, sh = area_ratio_range
     rl, rh = min_aspect_ratio, 1. / min_aspect_ratio
@@ -44,7 +44,37 @@ def get_random_eraser(p=1, area_ratio_range=[0.02, 0.4], min_aspect_ratio=0.3, m
                 break
 
         return image
-    return _random_erase
+    return _random_erase '''
+
+def get_random_eraser(p=1, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=255, pixel_level=False):
+    def eraser(input_img):
+        img_h, img_w, img_c = input_img.shape
+        p_1 = np.random.rand()
+
+        if p_1 > p:
+            return input_img
+
+        while True:
+            s = np.random.uniform(s_l, s_h) * img_h * img_w
+            r = np.random.uniform(r_1, r_2)
+            w = int(np.sqrt(s / r))
+            h = int(np.sqrt(s * r))
+            left = np.random.randint(0, img_w)
+            top = np.random.randint(0, img_h)
+
+            if left + w <= img_w and top + h <= img_h:
+                break
+
+        if pixel_level:
+            c = np.random.uniform(v_l, v_h, (h, w, img_c))
+        else:
+            c = np.random.uniform(v_l, v_h)
+
+        input_img[top:top + h, left:left + w, :] = c
+
+        return input_img
+
+    return eraser
 
 def gan_data(batch_size):
     crop_size = 108
@@ -98,13 +128,14 @@ def augument_data(batch_size):
 
 if __name__ == '__main__':
     
-    data_loader = iter(augument_data(23))
-    real_img, label1, fake_img, fake_img2, label0 = next(data_loader)
-    #img = mpimg.imread('/home/nevronas/dataset/img_align_celeba/1/011000.jpg')
+    #data_loader = iter(augument_data(23))
+    #real_img, label1, fake_img, fake_img2, label0 = next(data_loader)
+    img = mpimg.imread('/home/nevronas/dataset/img_align_celeba/1/011000.jpg')
     eraser = get_random_eraser()
-    #foo = eraser(fake_img)
+    fake_img = eraser(img)
+
     mpimg.imsave("./out.png", fake_img)
-    mpimg.imsave("./out2.png", fake_img2)
+    #mpimg.imsave("./out2.png", fake_img2)
     '''
     img = Image.fromarray(img, 'RGB')
     img.save('./foo.png')
