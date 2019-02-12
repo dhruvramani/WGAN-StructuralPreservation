f98e2cab2fa0a6dda9c7ccc4f34227db412f59ca
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 14:50:47 +0530

    checking eraser

--- dataset.py
+++ dataset.py
@@ -94,14 +94,15 @@ def augument_data(batch_size):
     return data_loader
 
 if __name__ == '__main__':
+    '''
     data_loader = iter(augument_data(23))
     eal_img, label1, fake_img, label0 = next(dataloader)
-
     '''
     img = mpimg.imread('/home/nevronas/dataset/img_align_celeba/1/011000.jpg')
-    mpimg.imsave("./out.png", img)
     eraser = get_random_eraser()
     img = eraser(img)
+    mpimg.imsave("./out.png", img)
+    '''
     img = Image.fromarray(img, 'RGB')
     img.save('./foo.png')
     img.show() '''
